ed8e18ed40158d0ebd960e6de2a8a76fcee44a9e
thederpykrafter <thederpykrafter@gmail.com>
2024-08-26 16:04:54 -0400

    handle keys

--- src/common.h
+++ src/common.h
@@ -1,3 +1,6 @@
 #include <curses.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdbool.h>
+
+#include "keys.h"
--- src/main.c
+++ src/main.c
@@ -1,4 +1,24 @@
 #include "common.h"
+#include <curses.h>
+
+int winWidth, winHeight;
+int keyPressed;
+bool running = true;
+
+void handleKeys(WINDOW *win) {
+  keyPressed = wgetch(win);
+  switch (keyPressed) {
+    case KEYS_Q:
+    case KEYS_ESC:
+      running = false;
+    case KEYS_CTRL_Q:
+      mvprintw(winWidth - 1, 0, "ctrl + q");
+      refresh();
+      break;
+    default:
+      break;
+  }
+}
 
 int main() {
   WINDOW *win = initscr();
@@ -6,21 +26,18 @@ int main() {
   noecho();
   keypad(win, true);
   nodelay(win, true);
+  nonl();
 
-  int pressed;
-
-  while (true) {
+  while (running) {
     clear();
 
-    mvprintw(0, 0, "Running");
-    refresh();
+    getmaxyx(win, winWidth, winHeight);
 
+    mvprintw(0, 0, "Running | X:%d Y:%d", winWidth, winHeight);
+    wrefresh(win);
+
+    handleKeys(win);
     napms(1000 / 60);
-
-    pressed = wgetch(win);
-    if (pressed == 113 || pressed == 27) { // q || esc
-      break;
-    }
   }
   endwin();
   return 0;
